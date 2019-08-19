2684db6cc42fa5300ec998372265f4445a094e9a
rchaser53 <tayoshizawa29@gmail.com>
2019-08-19 21:31:10 +0900

    setup

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,11 @@
 #[macro_use] extern crate lalrpop_util;
 
 pub mod calculator;
-// use calculator::TermParser;
+use calculator::ExprsParser;
 
-pub mod calculator2;
-// use calculator2::TermParser;
-
-#[test]
-fn calculator() {
-    assert!(calculator::TermParser::new().parse("22").is_ok());
-    assert!(calculator::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator::TermParser::new().parse("((22)").is_err());
-}
+pub mod ast;
 
 fn main() {
-    dbg!(calculator::TermParser::new().parse("22"));
-    dbg!(calculator2::TermParser::new().parse("22"));
+    let mut errors = Vec::new();
+    dbg!(ExprsParser::new().parse(&mut errors, "12 + 34 * 56"));
 }
