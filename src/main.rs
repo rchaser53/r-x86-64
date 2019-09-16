47a5aed413794acad5c86391c84f9da05055f076
rchaser53 <tayoshizawa29@gmail.com>
2019-09-16 20:50:30 +0900

    add statement

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ extern crate lalrpop_util;
 
 pub mod calculator;
 use calculator::ExprsParser;
-
+use calculator::StatementsParser;
 pub mod ast;
 
 fn main() {
     let mut errors = Vec::new();
     dbg!(ExprsParser::new().parse(&mut errors, "12 + 34 * 56"));
     dbg!(ExprsParser::new().parse(&mut errors, "AbcdE"));
+    dbg!(StatementsParser::new().parse(&mut errors, "22 = 123"));
 }
